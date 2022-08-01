9a781de209f8d35478dfe4cc9eaf26003760510a
Chigo <coolchigi0031@gmail.com>
2022-07-31 23:32:34 -0400

    updated src

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import services from "./Images/services.jpg";
 
+import Service from "./components/Services/Services";
+import Form from "./components/Form/Form";
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Services
+        </Typography>
+        <img src={services} alt="services" height="40"></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing="3"
+          >
+            <Grid item xs={12} sm={7}>
+              <Service />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/components/Services/Services.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Service from "./Service/Service";
+
+const Services = () => {
+  return (
+    <>
+      <h1>SERVICES</h1>
+
+      <Service />
+      <Service />
+    </>
+  );
+};
+
+export default Services;
